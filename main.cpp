aab5ba19190a76749439096d645a786f349d841b
XavYan <xavieryanquin@gmail.com>
2019-10-28 19:13:14 +0000

    Added '-b' option

--- main.cpp
+++ main.cpp
@@ -7,6 +7,8 @@
 // Modos
 std::string header = "include";
 std::string source = "src";
+std::string build = "build";
+
 std::string headerExt = "hpp";
 std::string sourceExt = "cpp";
 
@@ -21,6 +23,7 @@ void help () {
 	std::cout << "-c --header-ext\t\tSet the extension of the headers class. By default: hpp\n";
 	std::cout << "-s --source-dir\t\tSet the directory of the class source. By default: src\n";
 	std::cout << "-d --source-ext\t\tSet the extension of the class source. By default: cpp\n";
+	std::cout << "-b --build\t\tSet the directory where '.o' files are saved. By default: build\n";
 	std::cout << "-o --class\t\tSet the classes the program needs to be compiled with. Without it, it only compiles the main file.\n";
 	std::cout << "\t\t\tIMPORTANT: YOU NEED TO USE THIS ARGUMENT BEHIND THE OTHER.\n";
 }
@@ -34,6 +37,7 @@ int main(int argc, char *argv[]) {
 		{"source-dir", 	required_argument,	0,	's'},
 		{"header-ext",	required_argument,	0,	'c'},
 		{"source-ext",	required_argument,	0,	'd'},
+		{"build",		required_argument,	0,	'b'},
 		{"class",		no_argument,		0,	'o'},
 		{0, 0, 0, 0}
 	};
@@ -80,6 +84,12 @@ int main(int argc, char *argv[]) {
 					index++;
 				}
 				break;
+			case 'b':
+				if (optarg) {
+					build = optarg;
+					index++;
+				}
+				break;
 			default:
 				std::cerr << "Error with the arguments. Please use './gmk --help' if you don't know how to use this program.\n";
 				return -1;
@@ -93,19 +103,19 @@ int main(int argc, char *argv[]) {
 
 	std::string all = "all: "+source+"/main."+sourceExt;
 	for (int i = index; i < argc; i++) {
-		all += " build/"+std::string(argv[i])+".o";
+		all += " "+build+"/"+std::string(argv[i])+".o";
 	}
 	all += '\n';
 	
 	all += std::string("\t$(CC) $(CFLAGS) -o ")+nameOfFile+" "+all.substr(5)+"\n\n";
 	
 	for (int i = index; i < argc; i++) {
-		all += "build/"+std::string(argv[i])+".o: "+header+"/"+std::string(argv[i])+"."+headerExt+" "+source+"/"+std::string(argv[i])+"."+sourceExt+"\n";
-		all += std::string("\t$(CC) $(CFLAGS) -c -o build/")+std::string(argv[i])+".o "+source+"/"+std::string(argv[i])+"."+sourceExt+"\n\n";
+		all += build+"/"+std::string(argv[i])+".o: "+header+"/"+std::string(argv[i])+"."+headerExt+" "+source+"/"+std::string(argv[i])+"."+sourceExt+"\n";
+		all += std::string("\t$(CC) $(CFLAGS) -c -o "+build+"/")+std::string(argv[i])+".o "+source+"/"+std::string(argv[i])+"."+sourceExt+"\n\n";
 	}
 
 	all += ".PHONY: clean\n\n";
-	all += "clean:\n\trm -rf build/*.o\n";
+	all += "clean:\n\trm -rf "+build+"/*.o\n";
 
 	make << all;
 
